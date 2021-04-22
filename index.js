bb0c66e545237bcf466ae900ce6d679fa6ddbe44
Lanre Adelowo <yo@lanre.wtf>
2021-04-22 17:56:46 +0100

    add reference

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function Paystack(props, ref) {
                                 email: '${props.billingEmail}',
                                 amount: ${props.amount},
 				currency: "GHS",
-                                ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+                                ref: '${props.reference}'
                                 metadata: {
                                 custom_fields: [
                                         {
