6cd3864f296a78add3ad58d346fee3fc3387a327
Lanre Adelowo <yo@lanre.wtf>
2021-04-22 17:22:22 +0100

    add currency support

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ function Paystack(props, ref) {
                                 key: '${props.paystackKey}',
                                 email: '${props.billingEmail}',
                                 amount: ${props.amount},
-                                currency: ${props.currency},
                                 ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
                                 metadata: {
                                 custom_fields: [
